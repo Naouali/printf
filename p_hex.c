8a8c2bc8d8602e8da6a0b228d8861e3815208cba
doubix <adib.grouz@gmail.com>
2019-10-29 20:55:27 +0100

    fix

--- p_hex.c
+++ p_hex.c
@@ -1,6 +1,21 @@
 #include <stdarg.h>
 #include "holberton.h"
-
+/**
+ * p_base16 - prints number in base 16 recursively
+ * @value: The number to be printed
+ * @length: The amount of digits to be printed.
+ * Return: length.
+ */
+int p_base16(unsigned int value, int length)
+{
+	if (value / 16)
+		length = p_base16(value / 16, length + 1);
+	if (value % 16 < 10)
+		_putchar(value % 16 + 48);
+	else
+		_putchar(value % 16 + 55);
+	return (length);
+}
 /**
  * p_hex - writes in binary
  * @args: The name for va_list
@@ -12,6 +27,6 @@ int p_hex(va_list *args)
 	int length = 0;
 	unsigned int value = va_arg(*args, unsigned int);
 
-	length = basecnv(value, 16, length) + 1;
+	length = p_base16(value, length) + 1;
 	return (length);
 }
--- p_rot13.c
+++ p_rot13.c
@@ -15,10 +15,6 @@ int p_rot13(va_list *args)
 	char to[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	char *str = va_arg(*args, char *);
 
-	while (str[length])
-		length++;
-	if (str == NULL)
-		str = "(null)";
 	for (i = 0; str[i]; i++)
 	{
 		for (j = 0; j < 52; j++)
@@ -26,6 +22,7 @@ int p_rot13(va_list *args)
 			if (from[j] == str[i])
 			{
 				_putchar(to[j]);
+				length++;
 				break;
 			}
 		}
