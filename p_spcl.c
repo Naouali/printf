9badb91c017d149ea74d1d46709b1ab5a2e03b04
Naouali <elnaoualinebil@gmail.com>
2019-10-29 20:26:22 +0100

    m

--- p_spcl.c
+++ p_spcl.c
@@ -1,1 +1,15 @@
+#include <stdarg.h>
 #include "holberton.h"
+/**
+ * p_spcl - writes the character c
+ * @params: The name for va_list
+ *
+ * Return: On success 1.
+ * On error, -1 is returned.
+ */
+int p_spcl(va_list *params)
+{
+	char c = va_arg(*params, int);
+
+	return (c + 0);
+}
